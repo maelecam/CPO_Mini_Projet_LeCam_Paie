243aef79ad2ca547aa5959e5b2a81973859bdf6f
33604 <33604@10.3.60.222>
2024-11-29 08:55:05 +0100

    Début Projet MasterMind Paie et Le Cam

---
+++ CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/CPO_Mini_projet_LeCam_Paie_MasterMind.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.cpo_mini_projet_lecam_paie_mastermind;
+
+/**
+ *
+ * @author 33604
+ */
+public class CPO_Mini_projet_LeCam_Paie_MasterMind {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
