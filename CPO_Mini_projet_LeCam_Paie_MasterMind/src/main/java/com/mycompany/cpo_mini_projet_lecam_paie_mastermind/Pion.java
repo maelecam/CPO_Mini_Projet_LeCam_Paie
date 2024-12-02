863f393df9163dc125971891f4ce0062b5d778ca
33604 <33604@10.3.60.222>
2024-12-02 10:47:40 +0100

    Projet MasterMind Paie et Le Cam Classe Pion

--- CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Pion.java
+++ CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Pion.java
@@ -32,6 +32,7 @@ public class Pion {
         Pion PionJaune = new Pion('Y');
         Pion PionVert = new Pion('V');
         Pion PionBlanc = new Pion('W');
+        Pion PionNoir = new Pion('N');
         System.out.println("Pion rouge :" + PionRouge);
         System.out.println("Couleur du pion rouge : " + PionRouge.getCouleur());
         System.out.println("Pion bleue :" + PionBleue);
@@ -42,6 +43,8 @@ public class Pion {
         System.out.println("Pion de couleur vert : " + PionVert.getCouleur());
         System.out.println("Pion blanc :" + PionBlanc);
         System.out.println("Pion de couleur bleue : " + PionBlanc.getCouleur());
+        System.out.println("Pion noire : " + PionNoir);
+        System.out.println("Pion d ecouleur noire : " + PionNoir.getCouleur());
     }
 
     // Redéfinition de la méthode toString pour afficher la couleur
