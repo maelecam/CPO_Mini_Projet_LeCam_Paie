fc1fdb54d7adab4a5abeea271bef6f49cea545f1
33604 <33604@10.3.60.222>
2024-12-18 16:17:31 +0100

    Projet MasterMind Paie et Le Cam Menu du jeu

--- CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Menu.java
+++ CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Menu.java
@@ -46,6 +46,11 @@ public class Menu extends javax.swing.JFrame {
         });
 
         jButton2.setText("Régles");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Quitter");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -119,6 +124,12 @@ public class Menu extends javax.swing.JFrame {
         // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        afficher_regles Regle = new afficher_regles();
+        Regle.show();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
