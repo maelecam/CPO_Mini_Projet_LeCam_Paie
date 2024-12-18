517ea9f2eb3f7d75be965f2a3caee7ad9d5cb6d9
33604 <33604@10.3.60.222>
2024-12-18 16:03:53 +0100

    Projet MasterMind Paie et Le Cam Valider

--- CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Valider.java
+++ CPO_Mini_projet_LeCam_Paie_MasterMind/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Valider.java
@@ -40,6 +40,7 @@ public class Valider extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         Félicitation = new javax.swing.JLabel();
         Bravo = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,6 +50,13 @@ public class Valider extends javax.swing.JFrame {
         Félicitation.setText("Félicitation vous avez gagné");
 
         Bravo.setText("Vous avez trouvé la combinaison secréte");
+
+        jButton1.setText("RETOUR AU JEU");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -62,7 +70,10 @@ public class Valider extends javax.swing.JFrame {
                         .addGap(120, 120, 120))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(Bravo)
-                        .addGap(87, 87, 87))))
+                        .addGap(87, 87, 87))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(159, 159, 159))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -71,7 +82,9 @@ public class Valider extends javax.swing.JFrame {
                 .addComponent(Félicitation, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(Bravo)
-                .addContainerGap(156, Short.MAX_VALUE))
+                .addGap(67, 67, 67)
+                .addComponent(jButton1)
+                .addContainerGap(66, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -91,6 +104,11 @@ public class Valider extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,6 +156,7 @@ public class Valider extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Bravo;
     private javax.swing.JLabel Félicitation;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
