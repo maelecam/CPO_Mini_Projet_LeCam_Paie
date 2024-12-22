0371dbb93fa9b46bba2eff4780781c2610c07def
33604 <33604@10.3.60.222>
2024-12-22 16:33:30 +0100

    Projet MasterMind Paie et Le Cam Valider V1

--- com.mycompany_CPO_Mini_projet_LeCam_Paie_MasterMind_jar_1.0-SNAPSHOT_2/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Valider.java
+++ com.mycompany_CPO_Mini_projet_LeCam_Paie_MasterMind_jar_1.0-SNAPSHOT_2/src/main/java/com/mycompany/cpo_mini_projet_lecam_paie_mastermind/Valider.java
@@ -19,9 +19,8 @@ public class Valider extends javax.swing.JFrame {
     public Valider() {
         initComponents();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
-    }
 
+    }
 
     public void afficherResultats(int bienPlaces, int malPlaces) {
         Félicitation.setText("Nombre de Couleurs bien placés : " + bienPlaces);
@@ -41,6 +40,7 @@ public class Valider extends javax.swing.JFrame {
         Félicitation = new javax.swing.JLabel();
         Bravo = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -58,6 +58,13 @@ public class Valider extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Recommencer");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -70,10 +77,13 @@ public class Valider extends javax.swing.JFrame {
                         .addGap(120, 120, 120))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(Bravo)
-                        .addGap(87, 87, 87))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addGap(159, 159, 159))))
+                        .addGap(87, 87, 87))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(91, 91, 91)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton2)
+                .addGap(76, 76, 76))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -82,9 +92,11 @@ public class Valider extends javax.swing.JFrame {
                 .addComponent(Félicitation, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(Bravo)
-                .addGap(67, 67, 67)
-                .addComponent(jButton1)
-                .addContainerGap(66, Short.MAX_VALUE))
+                .addGap(65, 65, 65)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -109,6 +121,15 @@ public class Valider extends javax.swing.JFrame {
         // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        this.dispose(); // Ferme la fenêtre actuelle (si applicable)
+
+        // Relancez le jeu ou la fenêtre principale
+        FenetrePrincipale nouvelleFenetre = new FenetrePrincipale();
+        nouvelleFenetre.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -157,6 +178,7 @@ public class Valider extends javax.swing.JFrame {
     private javax.swing.JLabel Bravo;
     private javax.swing.JLabel Félicitation;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
